4ed7356f2770ff5a7edf07242f42705341befcdb
rlopezlu <rlopezlu@ucsc.edu>
2016-03-01 19:47:58 -0800

    add comment to on create

--- app/src/main/java/com/example/messiah/questjournal/MainActivity.java
+++ app/src/main/java/com/example/messiah/questjournal/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     // Firebase ref used to reference our Firebase backend
     public static Firebase ref;
 
-    //on create
+    //on create establish firebase connection
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

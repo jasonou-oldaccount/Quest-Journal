f3c42c4183e12e8ef9b182029ead093a2e8aa1a1
jasonou <jasonou95@gmail.com>
2016-03-01 19:23:19 -0800

    Firebase installed

--- app/src/main/java/com/example/messiah/questjournal/MainActivity.java
+++ app/src/main/java/com/example/messiah/questjournal/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.messiah.questjournal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
     }
 }

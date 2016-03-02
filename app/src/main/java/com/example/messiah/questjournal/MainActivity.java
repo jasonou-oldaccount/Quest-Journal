c5a9944b2cbb1c310db753a33e4afbced53b577a
jasonou <jasonou95@gmail.com>
2016-03-01 19:29:50 -0800

    Firebase ref created

--- app/src/main/java/com/example/messiah/questjournal/MainActivity.java
+++ app/src/main/java/com/example/messiah/questjournal/MainActivity.java
@@ -7,10 +7,14 @@ import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static Firebase ref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Firebase.setAndroidContext(this);
+
+        ref = new Firebase("https://questjournal.firebaseio.com/");
     }
 }

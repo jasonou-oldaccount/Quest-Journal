f2fd820dd3f4ad054781b6afe3a5c4505876995a
ankumar327 <kumar.anshika.5@gmail.com>
2016-03-05 15:59:21 -0800

    push quest to firebase

--- app/src/main/java/com/example/messiah/questjournal/QuestTabCreateTab.java
+++ app/src/main/java/com/example/messiah/questjournal/QuestTabCreateTab.java
@@ -3,6 +3,8 @@ package com.example.messiah.questjournal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
 
@@ -18,7 +20,30 @@ public class QuestTabCreateTab extends AppCompatActivity {
         String ref_quest = "https://questjournal.firebaseio.com/users/" + MainActivity.UID + "/CurrentQuests/";
         Firebase newQuest = new Firebase(ref_quest);
 
-        QuestObject createQuest = new QuestObject("Quest 1", 1, "This is the first quest", 01012016);
+        EditText title = (EditText) findViewById(R.id.title_input);
+        EditText diff = (EditText) findViewById(R.id.difficulty_input);
+        EditText desc = (EditText) findViewById(R.id.description_input);
+        EditText dateInput = (EditText) findViewById(R.id.deadline_input);
+
+        if (title.getText().toString().equals ("") | diff.getText().toString().equals ("") |
+                desc.getText().toString().equals("") | dateInput.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(), "Please fill out all fields.", Toast.LENGTH_SHORT).show();
+        }
+        int difficulty = 0;
+        int date = 0;
+        try {
+            difficulty = Integer.parseInt(diff.getText().toString());
+        } catch (NumberFormatException e) {
+            Toast.makeText(getApplicationContext(), "Please enter a valid difficulty level.", Toast.LENGTH_SHORT).show();
+        }
+        try {
+            date = Integer.parseInt(dateInput.getText().toString());
+        } catch (NumberFormatException e) {
+            Toast.makeText(getApplicationContext(), "Please enter a valid date.", Toast.LENGTH_SHORT).show();
+        }
+
+
+        QuestObject createQuest = new QuestObject(title.getText().toString(), difficulty, desc.getText().toString(), date);
 
         newQuest.push().setValue(createQuest);
     }
